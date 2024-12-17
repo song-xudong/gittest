62e494ccd682c39835bd546ac4232cd50c814c29
song-xudong <isxude@gmail.com>
2024-12-17 13:59:38 +0800

    first commit

---
+++ git-code.py
@@ -0,0 +1,12 @@
+echo "# gittest" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/song-xudong/gittest.git
+git push -u origin main
+
+
+git remote add origin https://github.com/song-xudong/gittest.git
+git branch -M main
+git push -u origin main
