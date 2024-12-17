86310cf85e30eda721ec699faacc3a12b673b1ce
song-xudong <isxude@gmail.com>
2024-12-17 14:03:27 +0800

    2222

--- git-code.py
+++ git-code.py
@@ -10,3 +10,13 @@ git push -u origin main
 git remote add origin https://github.com/song-xudong/gittest.git
 git branch -M main
 git push -u origin main
+
+#将所有内容提交
+git add . 
+
+#新增快照，名字为"first commit"
+git commit -m "first commit"
+
+#提交到github
+
+git push
